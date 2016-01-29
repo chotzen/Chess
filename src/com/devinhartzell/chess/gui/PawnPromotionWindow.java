340beac18fd1de100b2f3ee521034a0dd35f4d9f
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-01-28 21:08:36 -1000

    Add a pawn promotion window with no functionality

--- src/com/devinhartzell/chess/gui/PawnPromotionWindow.java
+++ src/com/devinhartzell/chess/gui/PawnPromotionWindow.java
@@ -1,14 +1,40 @@
 package com.devinhartzell.chess.gui;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class PawnPromotionWindow extends JFrame {
 	
 	private static final long serialVersionUID = -1686304609750241736L;
 
 	public PawnPromotionWindow() {
+		super("Pawn Promotion");
 		getContentPane().setLayout(null);
-		setSize(400, 200);
+		
+		JButton btnKnight = new JButton("Knight");
+		btnKnight.setBounds(6, 6, 117, 29);
+		getContentPane().add(btnKnight);
+		
+		JButton btnBishop = new JButton("Bishop");
+		btnBishop.setBounds(6, 35, 117, 29);
+		getContentPane().add(btnBishop);
+		
+		JButton btnRook = new JButton("Rook");
+		btnRook.setBounds(6, 63, 117, 29);
+		getContentPane().add(btnRook);
+		
+		JButton btnQueen = new JButton("Queen");
+		btnQueen.setBounds(6, 92, 117, 29);
+		getContentPane().add(btnQueen);
+		
+		JTextArea info = new JTextArea();
+		info.setOpaque(false);
+		info.setLineWrap(true);
+		info.setText("Since your pawn reached the far end of the board, you may upgrade it to a better piece.");
+		info.setBounds(135, 11, 159, 67);
+		getContentPane().add(info);
+		setSize(315, 155);
+		setVisible(true);
 	}
-
 }
